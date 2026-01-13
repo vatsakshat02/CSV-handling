f013664fcc12d7c860fb5eb2d96fc347e13ac432
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-14 00:40:45 +0530

    feat:completed week3 project

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ const sortDate = transaction.sort((a,b) => a.Date.getTime()-b.Date.getTime());
 
 // console.log(sortDate);
 
+//to calculate total credit and total debit of an user 
 function totalCredit(trans){
         const summary  = {};
         for(const txn of trans){
@@ -45,4 +46,93 @@ function totalCredit(trans){
         return Object.values(summary);
 }
 
-console.log(totalCredit(transaction))
+
+
+//identify the largest transaction per user
+function largestTrans(trans){
+    const summary = {};
+    
+
+    for(const txn of transaction){
+        const currentAmount =  Math.abs(txn.Amount)
+        const name = txn.AccountHolder;
+      if(!summary[name] || currentAmount>summary[name].LargestTransaction){
+        summary[name] = {
+            AccountHolder: name,
+            LargestTransaction: currentAmount
+            }
+        }
+     
+    }
+return Object.values(summary);
+}
+
+// console.log(largestTrans(transaction))
+
+//all the transaction where the remark contain salary
+function Salary(trans){
+   return trans.filter(txn => txn.Remarks.toLowerCase().includes('salary'))
+}
+
+// console.log(Salary(transaction))
+
+
+
+
+function SummariseTransacrions(summarise){
+    const summary = {};
+    for(const txn of summarise){
+        const name = txn.AccountHolder;
+        if(!summary[name]){
+            summary[name]= {
+                AccountHolder: name,
+                TotalCredit : 0,
+                TotalDebit : 0,
+                LargestTransaction: 0,
+                SalaryTransactions: []
+
+            }
+        }
+
+        if(txn.Type === "Credit"){
+            summary[name].TotalCredit+= txn.Amount;
+        }
+        else if(txn.Type === "Debit"){
+            summary[name].TotalDebit += txn.Amount;
+        }
+
+        const absAmount = Math.abs(txn.Amount)
+        if(absAmount>summary[name].LargestTransaction){
+            summary[name].LargestTransaction = absAmount;
+        }
+
+        if(txn.Remarks.toLowerCase().includes('salary')){
+            summary[name].SalaryTransactions.push(txn.TransactionID)
+        }
+    }
+    return Object.values(summary);
+}
+
+function SummaryCSV(summary){
+    const headers = [
+        "AccountHolder",
+        "TotalCredit",
+        "TotalDebit",
+        "LargestTransaction",
+        "SalaryTransactions"
+    ]
+    const rows = summary.map(obj => 
+        [
+        obj.AccountHolder,
+        obj.TotalCredit,
+        obj.TotalDebit,
+        obj.LargestTransaction,
+        obj.SalaryTransactions.join('|')].join(","))
+
+        const csv = [headers.join(','), ...rows].join('\n')
+        fs.writeFileSync('bank_summary.csv',csv);
+}
+
+const result = (SummariseTransacrions(transaction));
+
+console.log(SummaryCSV(result));
